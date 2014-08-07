cc376c20c8beca088c7165e506d0d59daff3e0bd
Rod Gau <rgau.ca@gmail.com>
2014-08-07 13:31:09 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'XphptuJGtyKWcW4GS0c1mZQI1LZ0OSWL92DC9Q47vvcOIr6H', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [98642123], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Goober App', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
