9ac748636bc61d160563830f7bc154b56cd117e9
Rod Gau <rgau.ca@gmail.com>
2014-08-30 03:44:17 -0600

    Update follows.js

--- follows.js
+++ follows.js
@@ -6,15 +6,15 @@
 		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataDuration	= '30days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0;
+		hideForm		= 1;
 	*/
 
 	var defaultKey		= 'XphptuJGtyKWcW4GS0c1mZQI1LZ0OSWL92DC9Q47vvcOIr6H', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [98642123], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Goober App', // Replaces Xively logo in the header
+		applicationName	= 'my test App', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
